facf8cd9d5a83c118396f24eb95299fadbff63a3
Mitchell Allen <mijallen@ucsc.edu>
2015-09-19 01:54:00 -0700

    implemented add and remove entries, transformable canvas class

--- interface.py
+++ interface.py
@@ -36,6 +36,7 @@ Proposed Design:
  -(DECLINED) use non-linear radius expansion for z-plane graph (radius squared?)
  -(DECLINED) graph s-plane from x of -c to c so that z-plane has radius from exp(-c) to exp(c)
  -graph frequency response by finding magnitude of various points in z- or s- plane
+   -use cubic or cosine interpolation, include pole/zero angles in the sample points
  -(in progress) have ability to add single real point or a conjugate pair
  -(in progress) restrict poles to within unit circle for z-plane and negative half for s-plane
  -add buttons to play original audio and new, filtered audio
@@ -138,6 +139,78 @@ sPlane = Tkinter.Canvas(top, bg="white", width=canvasWidth, height=canvasHeight)
 frame = Tkinter.Frame(top, width=canvasWidth, height=canvasHeight)
 
 '''
+Scale is determined by (canvasWidth-1) divided by domain of the graph
+Offset is simply the upper-left corner coordinate of the graph domain
+Padding is to compensate for the fact that canvas coordinates are offset by 2 pixels
+
+Need a class to represent roots as canvas elements
+will probably be self-sufficient (have reference to ComplexCanvas)
+Thinking of having a base RootCanvasItem with inheriting ZeroCanvasItem, PoleCanvasItem
+ZeroCanvasItem will have two ovals (one might not be used) it must update
+PoleCanvasItem will have four lines (two might not be used, two per X)
+each dictionary entry will have 2 ZeroCanvasItems and a Zero (or PoleCanvasItems, Pole)
+'''
+
+class ComplexCanvas:
+    def __init__(self, canvas, topLeftComplex, dimensionsComplex, paddingComplex = 2 + 2j):
+        self.canvas = canvas
+        self.offset = -topLeftComplex
+        canvasWidth = canvas.winfo_reqwidth() - 2 * paddingComplex.real
+        canvasHeight = canvas.winfo_reqheight() - 2 * paddingComplex.imag
+        self.scale = (canvasWidth - 1.0) / dimensionsComplex.real
+        self.scale -= (canvasHeight - 1.0) / dimensionsComplex.imag * 1j
+        self.padding = paddingComplex
+
+    def transformPoint(self, complexPoint):
+        canvasPoint = (complexPoint.real + self.offset.real) * self.scale.real
+        canvasPoint += (complexPoint.imag + self.offset.imag) * self.scale.imag * 1j
+        canvasPoint += self.padding
+        return canvasPoint
+
+    def inverseTransformPoint(self, canvasPoint):
+        canvasPoint -= self.padding
+        complexPoint = canvasPoint.real / self.scale.real - self.offset.real
+        complexPoint += (canvasPoint.imag / self.scale.imag - self.offset.imag) * 1j
+        return complexPoint
+
+    def addOvalItem(self, complexPointA, complexPointB, **options):
+        oval = self.canvas.create_oval(0, 0, 0, 0, options)
+        self.setItemPosition(oval, complexPointA, complexPointB)
+        return oval
+
+    def addLineItem(self, complexPointA, complexPointB, **options):
+        line = self.canvas.create_line(0, 0, 0, 0, options)
+        self.setItemPosition(line, complexPointA, complexPointB)
+        return line
+
+    def setItemPosition(self, item, complexPointA, complexPointB):
+        canvasPointA = self.transformPoint(complexPointA)
+        canvasPointB = self.transformPoint(complexPointB)
+        self.canvas.coords(item, canvasPointA.real, canvasPointA.imag,
+            canvasPointB.real, canvasPointB.imag)
+
+    def setItemPositionScreenSpace(self, item, complexPoint, scaleComplex):
+        canvasPoint = self.transformPoint(complexPoint)
+        self.canvas.coords(item, canvasPoint.real - scaleComplex.real,
+            canvasPoint.imag - scaleComplex.imag,
+            canvasPoint.real + scaleComplex.real,
+            canvasPoint.imag + scaleComplex.imag)
+
+    def configureItem(self, item, **options):
+        self.canvas.itemconfigure(item, options)
+
+    def deleteItem(self, item):
+        self.canvas.delete(item)
+
+zPlaneComplex = ComplexCanvas(zPlane, -2+2j, 4+4j)
+
+log20 = cmath.log(2.0)
+log02 = cmath.log(0.2)
+pi = cmath.pi
+
+sPlaneComplex = ComplexCanvas(sPlane, log02 + 3.25j, (log20 - log02) + 6.4j)
+
+'''
 # draw sound wave
 canvas.create_line(0.0, 256.0, 512.0, 256.0)
 widthCompressionFactor = 512.0 / len(normalizedSoundFloats)
@@ -164,15 +237,15 @@ def makeActive(entry):
 
     if (activeEntry != None):
         key = id(activeEntry)
-        zPlane.itemconfig(entryDictionary[key][ZPLANEOVAL], outline="black")
-        sPlane.itemconfig(entryDictionary[key][SPLANEOVAL], outline="black")
+        zPlaneComplex.configureItem(entryDictionary[key][ZPLANEOVAL], outline="black")
+        sPlaneComplex.configureItem(entryDictionary[key][SPLANEOVAL], outline="black")
         activeEntry.config(bg="white")
 
     activeEntry = entry
 
     key = id(activeEntry)
-    zPlane.itemconfig(entryDictionary[key][ZPLANEOVAL], outline="green")
-    sPlane.itemconfig(entryDictionary[key][SPLANEOVAL], outline="green")
+    zPlaneComplex.configureItem(entryDictionary[key][ZPLANEOVAL], outline="green")
+    sPlaneComplex.configureItem(entryDictionary[key][SPLANEOVAL], outline="green")
     activeEntry.config(bg="green")
 
 def setActivePosition(position):
@@ -183,174 +256,103 @@ def setActivePosition(position):
 def entryClick(event):
     makeActive(event.widget)
 
-# not needed?
-
-#entryList = []
-#for index in range(0, len(zeroList)):
-#    entry = Tkinter.Entry(frame)
-#    entryList.append(entry)
-#    entry.grid(row = index)
-#    entry.bind("<Button-1>", entryClick)
-
-diagonal = 1.0 + 1.0j
-
-'''
-Scale is determined by (canvasWidth-1) divided by domain of the graph
-Offset is simply the upper-left corner coordinate of the graph domain
-Padding is to compensate for the fact that canvas coordinates are offset by 2 pixels
-'''
-
-# can probably be used for a class that contains a canvas, abstracts away transforms
-
-canvas1Scale = (canvasWidth - 1.0) / 4.0 - (canvasWidth - 1.0) / 4.0 * 1j
-canvas1Offset = -2.0 + 2.0j
-canvas1Padding = 2.0 * diagonal
-
-def canvas1InverseTransform(canvasPoint):
-    canvasPoint -= canvas1Padding
-    complexPoint = canvasPoint.real / canvas1Scale.real + canvas1Offset.real
-    complexPoint += (canvasPoint.imag / canvas1Scale.imag + canvas1Offset.imag) * 1j
-    return complexPoint
-
-def canvas1Transform(complexPoint):
-    canvasPoint = (complexPoint.real - canvas1Offset.real) * canvas1Scale.real
-    canvasPoint += ( (complexPoint.imag - canvas1Offset.imag) * canvas1Scale.imag ) * 1j
-    canvasPoint += canvas1Padding
-    return canvasPoint
-
-def getCanvas1BoundingBoxCoords(complexA, complexB):
-    canvasPointA = canvas1Transform(complexA)
-    canvasPointB = canvas1Transform(complexB)
-    return canvasPointA.real, canvasPointA.imag, canvasPointB.real, canvasPointB.imag
-
-# modified to better show negative reals from Z-plane at top of S-plane graph
-
-canvas2Scale = (canvasWidth - 1.0) / (cmath.log(2.0) - cmath.log(0.2))
-#canvas2Scale -= (canvasHeight - 1.0) / (2.0 * cmath.pi) * 1j
-canvas2Scale -= (canvasHeight - 1.0) / 6.4 * 1j
-#canvas2Offset = cmath.log(0.2) + cmath.pi * 1j
-canvas2Offset = cmath.log(0.2) + 3.25 * 1j
-canvas2Padding = 2.0 * diagonal
-
-def canvas2Transform(complexPoint):
-    canvasPoint = (complexPoint.real - canvas2Offset.real) * canvas2Scale.real
-    canvasPoint += ( (complexPoint.imag - canvas2Offset.imag) * canvas2Scale.imag ) * 1j
-    canvasPoint += canvas2Padding
-    return canvasPoint
-
-def canvas2InverseTransform(canvasPoint):
-    canvasPoint -= canvas2Padding
-    complexPoint = canvasPoint.real / canvas2Scale.real + canvas2Offset.real
-    complexPoint += (canvasPoint.imag / canvas2Scale.imag + canvas2Offset.imag) * 1j
-    return complexPoint
-
-def getCanvas2BoundingBoxCoords(complexA, complexB):
-    canvasPointA = canvas2Transform(complexA)
-    canvasPointB = canvas2Transform(complexB)
-    return canvasPointA.real, canvasPointA.imag, canvasPointB.real, canvasPointB.imag
-
 # create Z-plane graph of three circles and a real axis
 
-unitCircleCoords = getCanvas1BoundingBoxCoords( 1.0 * -diagonal, 1.0 * diagonal )
-maximumCircleCoords = getCanvas1BoundingBoxCoords( 2.0 * -diagonal, 2.0 * diagonal )
-minimumCircleCoords = getCanvas1BoundingBoxCoords( 0.2 * -diagonal, 0.2 * diagonal )
-realAxisCoords = getCanvas1BoundingBoxCoords( -2.0, +2.0 )
-
-unitCircle = zPlane.create_oval(unitCircleCoords, fill=None)
-maximumCircle = zPlane.create_oval(maximumCircleCoords, fill=None, outline="blue")
-minimumCircle = zPlane.create_oval(minimumCircleCoords, fill=None, outline="red")
-realAxis = zPlane.create_line(realAxisCoords, stipple="gray75")
+unitCircle = zPlaneComplex.addOvalItem(-1-1j, 1+1j, outline="black")
+maximumCircle = zPlaneComplex.addOvalItem(-2-2j, 2+2j, outline="blue")
+minimumCircle = zPlaneComplex.addOvalItem(-0.2-0.2j, 0.2+0.2j, outline="red")
+realAxis = zPlaneComplex.addLineItem(-2.0, 2.0, stipple="gray75")
 
 # create S-plane graph of three radius lines and three real axis lines
 
-log20 = cmath.log(2.0)
-log02 = cmath.log(0.2)
+unitLine = sPlaneComplex.addLineItem(0 + pi*1j, 0 - pi*1j, fill="black")
+maximumLine = sPlaneComplex.addLineItem(log20 + pi*1j, log20 - pi*1j, fill="blue")
+minimumLine = sPlaneComplex.addLineItem(log02 + pi*1j, log02 - pi*1j, fill="red")
+realAxisZero = sPlaneComplex.addLineItem(log02, log20, stipple="gray75")
+realAxisPositivePi = sPlaneComplex.addLineItem(log02 + pi*1j, log20 + pi*1j, stipple="gray75")
+realAxisNegativePi = sPlaneComplex.addLineItem(log02 - pi*1j, log20 - pi*1j, stipple="gray75")
 
-unitLineCoords = getCanvas2BoundingBoxCoords( +cmath.pi * 1.0j, -cmath.pi * 1.0j )
-maximumLineCoords = getCanvas2BoundingBoxCoords( log20 + cmath.pi * 1.0j, log20 - cmath.pi * 1.0j )
-minimumLineCoords = getCanvas2BoundingBoxCoords( log02 + cmath.pi * 1.0j, log02 - cmath.pi * 1.0j )
-
-realAxisZeroCoords = getCanvas2BoundingBoxCoords( log02, log20 )
-realAxisPositivePiCoords = getCanvas2BoundingBoxCoords( log02 + cmath.pi * 1j, log20 + cmath.pi * 1j )
-realAxisNegativePiCoords = getCanvas2BoundingBoxCoords( log02 - cmath.pi * 1j, log20 - cmath.pi * 1j )
-
-unitLine = sPlane.create_line(unitLineCoords)
-maximumLine = sPlane.create_line(maximumLineCoords, fill="blue")
-minimumLine = sPlane.create_line(minimumLineCoords, fill="red")
-realAxisZero = sPlane.create_line(realAxisZeroCoords, stipple="gray75")
-realAxisPositivePi = sPlane.create_line(realAxisPositivePiCoords, stipple="gray75")
-realAxisNegativePi = sPlane.create_line(realAxisNegativePiCoords, stipple="gray75")
-
-def placeCanvasOval(canvas, oval, point, radius):
-    canvas.coords(oval, point.real - radius, point.imag - radius, point.real + radius, point.imag + radius)
-
-'''
-currently builds collection of poles and zeros based on zeroList
-click on an entry in the panel on the right to make it active
-then click inside either canvas to move it
-must click an entry before clicking canvas, otherwise activeEntry == None
-
-first entry is an unpaired Zero, can move in [-2,-0.2] and [+0.2,+2]
-second entry is an unpaired Pole, can move in [-1,-0.2] and [+0.2,+1]
-third entry is a "paired" Pole, can move so that magnitude in [0.2,1]
-
-still need to have two diagonal lines for Poles, rather than and oval
-still need to draw the paired pole/zero's conjugate reflected across real axis
-still need to implement ability to add/remove entries
-'''
-
-for index in range(0, len(zeroList)):
+def addNewEntry(position, isZero):
     entry = Tkinter.Entry(frame)
-    entry.grid(row = index)
+    #entry.grid(row = len(entryDictionary))
     entry.bind("<Button-1>", entryClick)
 
-    #key = id(entryList[index])
+    for entryIndex in range(0, len(frame.winfo_children())):
+        frame.winfo_children()[entryIndex].grid(row = entryIndex)
+
     key = id(entry)
 
-    if (index == 0):
-        entryRoot = Zero(zeroList[index])
+    if (isZero):
+        entryRoot = Zero(position)
     else:
-        entryRoot = Pole(zeroList[index])
-    if (index == 2):
-        entryRoot.pair = True
+        entryRoot = Pole(position)
 
-    zPlaneOval = zPlane.create_oval(0, 0, 0, 0, fill=None)
-    zPlanePosition = canvas1Transform( entryRoot.getPosition() )
-    placeCanvasOval(zPlane, zPlaneOval, zPlanePosition, 5)
+    zPlaneOval = zPlaneComplex.addOvalItem(0,0)
+    zPlaneComplex.setItemPositionScreenSpace(zPlaneOval, entryRoot.getPosition(), 5+5j)
 
-    sPlaneOval = sPlane.create_oval(0, 0, 0, 0, fill=None)
-    sPlanePosition = canvas2Transform( cmath.log(entryRoot.getPosition()) )
-    placeCanvasOval(sPlane, sPlaneOval, sPlanePosition, 5)
+    sPlaneOval = sPlaneComplex.addOvalItem(0,0)
+    sPlaneComplex.setItemPositionScreenSpace(sPlaneOval, cmath.log(entryRoot.getPosition()), 5+5j)
 
     entryDictionary[key] = (zPlaneOval, sPlaneOval, entryRoot)
 
+def removeActiveEntry():
+    global activeEntry
+
+    key = id(activeEntry)
+
+    zPlaneComplex.deleteItem(entryDictionary[key][ZPLANEOVAL])
+    sPlaneComplex.deleteItem(entryDictionary[key][SPLANEOVAL])
+    del entryDictionary[key]
+
+    activeEntry.destroy()
+    activeEntry = None
+
+for index in range(0, len(zeroList)):
+    addNewEntry(zeroList[index], True)
+
+addNewEntry(0.5, False)
 #makeActive(entryList[0])
 
 def updatePosition(position):
     position = round(position.real, 5) + round(position.imag, 5) * 1j
     position = setActivePosition(position)
 
-    canvas1Point = canvas1Transform(position)
-    canvas2Point = canvas2Transform(cmath.log(position))
+    zPlanePoint = position
+    sPlanePoint = cmath.log(position)
+
+    print zPlanePoint
 
     key = id(activeEntry)
-    placeCanvasOval(zPlane, entryDictionary[key][ZPLANEOVAL], canvas1Point, 5)
-    placeCanvasOval(sPlane, entryDictionary[key][SPLANEOVAL], canvas2Point, 5)
+    zPlaneComplex.setItemPositionScreenSpace(entryDictionary[key][ZPLANEOVAL], zPlanePoint, 5+5j)
+    sPlaneComplex.setItemPositionScreenSpace(entryDictionary[key][SPLANEOVAL], sPlanePoint, 5+5j)
+
+    print zPlane.coords(entryDictionary[key][ZPLANEOVAL])
 
     activeEntry.delete(0, Tkinter.END)
     activeEntry.insert(0, repr(position))
 
 def zPlaneMouseClick(event):
-    complexMouse = canvas1InverseTransform(event.x + event.y * 1j)
+    complexMouse = zPlaneComplex.inverseTransformPoint(event.x + event.y * 1j)
     updatePosition(complexMouse)
 
 def sPlaneMouseClick(event):
-    complexMouse = canvas2InverseTransform(event.x + event.y * 1j)
+    complexMouse = sPlaneComplex.inverseTransformPoint(event.x + event.y * 1j)
     updatePosition(cmath.exp(complexMouse))
+
+def rclick(event):
+    removeActiveEntry()
+    print frame.winfo_children()
+
+def mclick(event):
+    addNewEntry(0.5, True)
+    print frame.winfo_children()
 
 zPlane.bind("<Button-1>", zPlaneMouseClick)
 #zPlane.bind("<Motion>", zPlaneMouseClick)
 sPlane.bind("<Button-1>", sPlaneMouseClick)
+
+zPlane.bind("<Button-2>", mclick)
+zPlane.bind("<Button-3>", rclick)
 
 zPlane.grid(row=0, column=0)
 sPlane.grid(row=0, column=1)
